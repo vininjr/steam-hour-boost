5553e49008e02e0364b5ef755fac84d67bbc8577
Marcus Vinicius M. Melo <viniips@alu.ufc.br>
2020-05-18 17:37:06 -0300

    Delete package-lock.json

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# steam-hour-boost
+# Steam Hour Booste
+
+## Installation [Manual]
+```
+git clone https://github.com/vininjr/steam-hour-boost.git
+cd steam-hour-boost
+npm install
+```
+
+## Startup [Manual]
+```
+node idleConfig.js <YOUR_USERNAME> <YOUR_PASSWORD>
+```
--- idlerConfig.js
+++ idlerConfig.js
@@ -3,7 +3,7 @@ var steamClientFactory = require('./steamClient.js');
 var configsArray = [], config, botArray = [];
 var argv = process.argv.slice(2);
 
-if (!argv[1] || !argv[2]) {
+if (!argv[0] || !argv[1]) {
   console.log("Parameters not found");
   return;
 }
@@ -12,11 +12,12 @@ config = {};
 config.username = argv[0]; // YOUR_USERNAME
 config.password = argv[1]; // YOUR_PASSWORD
 config.sharedSecret = argv[2] || ''; // OPTIONAL
-config.games = [730] // 730 = CSGO. Exemple [730,107410] // 107410 correspond a Arma 3.
+config.games = [730]; // 730 = CSGO. Exemple [730,107410] // 107410 correspond a Arma 3.
 configsArray.push(config);
 
 for (index = 0; index < configsArray.length; index++) {
-  var config = configsArray[index], bot = steamClientFactory.buildBot(config);
+  var config = configsArray[index]; 
+  var bot = steamClientFactory.buildBot(config);
   bot.doLogin();
   botArray.push(bot);
 }
